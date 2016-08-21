136f29384d8815c774d48c1228d7b834c5c3ed27
IanH <ian_web1@hubbertz.de>
2016-08-21 09:57:56 +0200

    Adapted to latest Homie release

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,62 @@
 #include <Homie.h>
 #include <RGBWNode.h>
 #include <SensorNode.h>
+#include <LoggerNode.h>
 
 /* includes for Libraries, so platformio can find them */
 #include <Wire.h>
 #include <Sensors.h>
 
+#include <InputController.h>
+
+#include <SSD1306.h>
+
 RGBWNode rgbn;
 SensorNode sensor;
 
+SSD1306Wire display(0x3c, SDA, SCL);
+OLEDDisplayUi ui(&display);
+InputController ictrl;
+
+
 void setup() {
+	Serial.begin(115200);
+	Serial.println("Setup");
+	Serial.flush();
 	Homie.disableResetTrigger();
+	//Homie.setLoggingPrinter(&display);
+	Homie.setLoggingPrinter(&Serial);
+	display.setLogBuffer(4,200);
+	LN.setLoglevel(LoggerNode::DEBUG);
+
+	ui.setTargetFPS(30);
+
+//	// Customize the active and inactive symbol
+//	ui.setActiveSymbol(activeSymbol);
+//	ui.setInactiveSymbol(inactiveSymbol);
+
+	// You can change this to
+	// TOP, LEFT, BOTTOM, RIGHT
+	ui.setIndicatorPosition(BOTTOM);
+
+	// Defines where the first frame is located in the bar.
+	ui.setIndicatorDirection(LEFT_RIGHT);
+
+	// You can change the transition that is used
+	// SLIDE_LEFT, SLIDE_RIGHT, SLIDE_UP, SLIDE_DOWN
+	ui.setFrameAnimation(SLIDE_LEFT);
+	ui.disableAutoTransition();
+	ui.disableAllIndicators();
+	ui.init();
+
+	display.flipScreenVertically();
+
 	Homie.setup();
 }
 
 void loop() {
+	ictrl.loop();
 	Homie.loop();
+	ui.update();
+
 }
